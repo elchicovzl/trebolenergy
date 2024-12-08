1fd51d05b4f579741ea7bc28aea2d0764c2e0723
Stefan Djikic <stefandjikic@outlook.com>
2024-12-08 13:08:36 +0100

    Refactor main layout in Home component for improved alignment

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import RSVPForm from "./_components/RSVPForm";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="flex items-center">
       <RSVPForm />
     </main>
   );
