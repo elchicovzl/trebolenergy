303a28737626e850c3988f55f70d507c40b37fb6
Stefan Djikic <stefandjikic@outlook.com>
2024-12-11 02:05:20 +0100

    Enhance RSVP management page: add navigation link to home and improve layout with icon integration

--- app/(pages)/admin/rsvps/page.tsx
+++ app/(pages)/admin/rsvps/page.tsx
@@ -3,6 +3,8 @@ import { signOut } from "@/app/actions/auth";
 import { redirect } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { RSVPTable } from "@/app/_components/RSVPTable";
+import { House } from "lucide-react";
+import Link from "next/link";
 
 export default async function RSVPsPage() {
   const { success, data, message } = await getRSVPs();
@@ -18,11 +20,19 @@ export default async function RSVPsPage() {
     <div className="container mx-auto mt-8 p-4">
       <div className="flex justify-between items-center mb-6">
         <h1 className="text-2xl font-bold">All RSVPs</h1>
-        <form action={signOut}>
-          <Button variant="outline" type="submit">
-            Sign Out
-          </Button>
-        </form>
+        <div className="flex items-center gap-2">
+          <Link href="/">
+            <Button variant="outline">
+              {/* <LucideStepBack /> */}
+              <House />
+            </Button>
+          </Link>
+          <form action={signOut}>
+            <Button variant="outline" type="submit">
+              Sign Out
+            </Button>
+          </form>
+        </div>
       </div>
       <RSVPTable data={data || []} />
     </div>
